596847cdc837c5b572d48e1063ce0adec3223d4e
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-06-05 11:16:40 -0400

    updated module registration

--- dist/safe.accessors.js
+++ dist/safe.accessors.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 /**
  * safe.accessors (c) 2015 Peter Pavlovich <pavlovich@gmail.com>
  *  safe.accessors is freely distributable under the terms of the MIT license.
@@ -6,40 +7,103 @@
  *  Version '1.0.0'
  */
 
-'use strict';
+;(function() {
 
-var _ = require('lodash');
+  var _ = require('lodash');
 
-function sa(value) {
-  /* jshint validthis: true */
-  if (!(this instanceof sa)) {
-    return new sa(value);
+  /** Used to determine if values are of the language type `Object`. */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  /** Detect free variable `exports`. */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module`. */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect free variable `global` from Node.js. */
+  var freeGlobal = freeExports && freeModule && typeof global == 'object' && global;
+
+  /** Detect free variable `self`. */
+  var freeSelf = objectTypes[typeof self] && self && self.Object && self;
+
+  /** Detect free variable `window`. */
+  var freeWindow = objectTypes[typeof window] && window && window.Object && window;
+
+  /** Detect the popular CommonJS extension `module.exports`. */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  /**
+   * Used as a reference to the global object.
+   *
+   * The `this` value is used if it is the global object to avoid Greasemonkey's
+   * restricted `window` object, otherwise the `window` object is used.
+   */
+  var root = freeGlobal || ((freeWindow !== (this && this.window)) && freeWindow) || freeSelf || this;
+
+  function sa(value) {
+    /* jshint validthis: true */
+    if (!(this instanceof sa)) {
+      return new sa(value);
+    }
+    this._wrapped = value;
   }
-  this._wrapped = value;
-}
 
-sa.VERSION = '1.0.0';
+  sa.VERSION = '1.0.0';
 
-sa.safeGet  = require('./safeGet');
-sa.safeSet  = require('./safeSet');
-sa.safeCall = require('./safeCall');
-sa.isVoid   = require('./isVoid');
-sa.install  = function install(obj){
-  obj.safeGet = sa.safeGet;
-  obj.safeSet = sa.safeSet;
-  obj.safeCall = sa.safeCall;
-  obj.isVoid = sa.isVoid;
-};
+  sa.safeGet  = require('./safeGet');
+  sa.safeSet  = require('./safeSet');
+  sa.safeCall = require('./safeCall');
+  sa.isVoid   = require('./isVoid');
+  sa.install  = function install(obj){
+    obj.safeGet = sa.safeGet;
+    obj.safeSet = sa.safeSet;
+    obj.safeCall = sa.safeCall;
+    obj.isVoid = sa.isVoid;
+  };
 
-// Implement chaining
-sa.prototype = {
-  value: function value() {
-    return this._wrapped;
+  // Implement chaining
+  sa.prototype = {
+    value: function value() {
+      return this._wrapped;
+    }
+  };
+
+
+  // Some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose lodash to the global object when an AMD loader is present to avoid
+    // errors in cases where lodash is loaded by a script tag and not intended
+    // as an AMD module. See http://requirejs.org/docs/errors.html#mismatch for
+    // more details.
+    root.sa = sa;
+
+    // Define as an anonymous module so, through path mapping, it can be
+    // referenced as the "underscore" module.
+    define(function() {
+      return sa;
+    });
   }
-};
+  // Check for `exports` after `define` in case a build optimizer adds an `exports` object.
+  else if (freeExports && freeModule) {
+    // Export for Node.js or RingoJS.
+    if (moduleExports) {
+      (freeModule.exports = sa).sa = sa;
+    }
+    // Export for Narwhal or Rhino -require.
+    else {
+      freeExports.sa = sa;
+    }
+  }
+  else {
+    // Export for a browser or Rhino.
+    root.sa = sa;
+  }
 
-module.exports = sa;
-
+}.call(this));
+}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./isVoid":2,"./safeCall":4,"./safeGet":5,"./safeSet":6,"lodash":3}],2:[function(require,module,exports){
 /**
  * Created by peter.pavlovich on 6/4/15.
--- index.js
+++ index.js
@@ -5,36 +5,99 @@
  *  Version '1.0.0'
  */
 
-'use strict';
+;(function() {
 
-var _ = require('lodash');
+  var _ = require('lodash');
 
-function sa(value) {
-  /* jshint validthis: true */
-  if (!(this instanceof sa)) {
-    return new sa(value);
+  /** Used to determine if values are of the language type `Object`. */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  /** Detect free variable `exports`. */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module`. */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect free variable `global` from Node.js. */
+  var freeGlobal = freeExports && freeModule && typeof global == 'object' && global;
+
+  /** Detect free variable `self`. */
+  var freeSelf = objectTypes[typeof self] && self && self.Object && self;
+
+  /** Detect free variable `window`. */
+  var freeWindow = objectTypes[typeof window] && window && window.Object && window;
+
+  /** Detect the popular CommonJS extension `module.exports`. */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  /**
+   * Used as a reference to the global object.
+   *
+   * The `this` value is used if it is the global object to avoid Greasemonkey's
+   * restricted `window` object, otherwise the `window` object is used.
+   */
+  var root = freeGlobal || ((freeWindow !== (this && this.window)) && freeWindow) || freeSelf || this;
+
+  function sa(value) {
+    /* jshint validthis: true */
+    if (!(this instanceof sa)) {
+      return new sa(value);
+    }
+    this._wrapped = value;
   }
-  this._wrapped = value;
-}
 
-sa.VERSION = '1.0.0';
+  sa.VERSION = '1.0.0';
 
-sa.safeGet  = require('./safeGet');
-sa.safeSet  = require('./safeSet');
-sa.safeCall = require('./safeCall');
-sa.isVoid   = require('./isVoid');
-sa.install  = function install(obj){
-  obj.safeGet = sa.safeGet;
-  obj.safeSet = sa.safeSet;
-  obj.safeCall = sa.safeCall;
-  obj.isVoid = sa.isVoid;
-};
+  sa.safeGet  = require('./safeGet');
+  sa.safeSet  = require('./safeSet');
+  sa.safeCall = require('./safeCall');
+  sa.isVoid   = require('./isVoid');
+  sa.install  = function install(obj){
+    obj.safeGet = sa.safeGet;
+    obj.safeSet = sa.safeSet;
+    obj.safeCall = sa.safeCall;
+    obj.isVoid = sa.isVoid;
+  };
 
-// Implement chaining
-sa.prototype = {
-  value: function value() {
-    return this._wrapped;
+  // Implement chaining
+  sa.prototype = {
+    value: function value() {
+      return this._wrapped;
+    }
+  };
+
+
+  // Some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose lodash to the global object when an AMD loader is present to avoid
+    // errors in cases where lodash is loaded by a script tag and not intended
+    // as an AMD module. See http://requirejs.org/docs/errors.html#mismatch for
+    // more details.
+    root.sa = sa;
+
+    // Define as an anonymous module so, through path mapping, it can be
+    // referenced as the "underscore" module.
+    define(function() {
+      return sa;
+    });
   }
-};
+  // Check for `exports` after `define` in case a build optimizer adds an `exports` object.
+  else if (freeExports && freeModule) {
+    // Export for Node.js or RingoJS.
+    if (moduleExports) {
+      (freeModule.exports = sa).sa = sa;
+    }
+    // Export for Narwhal or Rhino -require.
+    else {
+      freeExports.sa = sa;
+    }
+  }
+  else {
+    // Export for a browser or Rhino.
+    root.sa = sa;
+  }
 
-module.exports = sa;
+}.call(this));
