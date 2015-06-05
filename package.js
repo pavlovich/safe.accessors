d1877ac96c5b833e5fbcb31bf14bbbfa220fd044
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-06-05 02:42:51 -0400

    update travis config

--- package.js
+++ package.js
@@ -1,16 +1,15 @@
 // package metadata file for Meteor.js
 
 Package.describe({
-  name: 'safeaccessors:safeaccessors',
-  summary: 'safeaccessors (official): Safely get and set attribute values and execute functions.',
+  name: 'safe.accessors:safe.accessors',
+  summary: 'safe.accessors (official): Safely get and set attribute values and execute functions.',
   version: '1.0.0',
   git: 'https://github.com/pavlovich/safeaccessors.git',
   documentation: 'README.markdown'
 });
 
-
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
-  api.addFiles(['meteor-pre.js','dist/underscore.string.js','meteor-post.js']);
-  api.export("s");
+  api.addFiles(['dist/safeaccessors.js']);
+  api.export("sa");
 });
